951c03dce687a72cc179212d1476c843bf8c2bf5
Chetan Ananthu(UST <chetanananthu@gmail.com>
2024-12-05 10:20:58 +0530

    storing appointments in the localstorage

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -21,10 +21,13 @@ export class AppointmentListComponent {
       this.appointments.push(newAppoint);
       this.newAppointmentTitle="";
       this.newAppointmentDate=new Date();
+
+      localStorage.setItem("appointments",JSON.stringify(this.appointments))
     }
   }
 
   deleteAppointment(index:number){
     this.appointments.splice(index,1);
+    localStorage.setItem("appointments",JSON.stringify(this.appointments))
   }
 }
