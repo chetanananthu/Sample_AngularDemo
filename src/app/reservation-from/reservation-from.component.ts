c6e5521eceb88174e618a7475e997d4ae7886a65
Chetan Ananthu(UST <chetanananthu@gmail.com>
2024-12-09 12:29:17 +0530

    creating the reservation form

--- src/app/reservation-from/reservation-from.component.ts
+++ src/app/reservation-from/reservation-from.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { FormBuilder,FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-from',
   templateUrl: './reservation-from.component.html',
   styleUrls: ['./reservation-from.component.css']
 })
-export class ReservationFromComponent {
 
+export class ReservationFromComponent {
+  reservationForm :FormGroup =new FormGroup({});
+
+  onSubmit(){
+    
+  }
 }
