a7f23977fdf0e3721d89b042e571e72f38fb3b07
Chetan Ananthu(UST <chetanananthu@gmail.com>
2024-12-10 15:43:30 +0530

    combining components from different module

--- src/app/reservation/reservation.module.ts
+++ src/app/reservation/reservation.module.ts
@@ -4,6 +4,7 @@ import { ReservationFromComponent } from '../reservation-from/reservation-from.c
 import { ReservationListComponent } from '../reservation-list/reservation-list.component';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { HomeModule } from '../home/home.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { RouterModule } from '@angular/router';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule
+    RouterModule,
+    HomeModule
   ]
 })
 export class ReservationModule { }
