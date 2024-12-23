9d5b09d385276e24af0d222a8923dc63ebe3b1b4
Chetan Ananthu(UST <chetanananthu@gmail.com>
2024-12-24 00:34:39 +0530

    calling mock-api using http client and observables

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Reservation } from '../models/reservation';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
+  private apiUrl="https://fd554605-4f1c-47d0-b8fe-6a1531588da5.mock.pstmn.io"
   private reservations:Reservation[]=[];
 
-  constructor(){
-    let savedReservations =localStorage.getItem("reservations");
-    this.reservations=savedReservations ? JSON.parse(savedReservations) : [];
-  }
+  constructor(private http:HttpClient){}
+
   //CRUD
 
-  getReservations():Reservation[]{
-    return this.reservations;
+  getReservations():Observable<Reservation[]>{
+    return this.http.get<Reservation[]>(this.apiUrl+"/reservations");
   }
 
   getReservation(id:string):Reservation | undefined{
@@ -25,19 +26,16 @@ export class ReservationService {
   addReservation(reservation:Reservation):void{
     reservation.id=Date.now().toString();
     this.reservations.push(reservation);
-    localStorage.setItem("reservations",JSON.stringify(this.reservations));
   }
 
 deleteReservation(id:string):void{
   let index=this.reservations.findIndex(res=>res.id===id);
   this.reservations.splice(index,1);
-  localStorage.setItem("reservations",JSON.stringify(this.reservations));
 }
 
 updateReservation(id:string,updateReservation:Reservation):void{
   let index=this.reservations.findIndex(res=>res.id===id);
   this.reservations[index]=updateReservation;
-  localStorage.setItem("reservations",JSON.stringify(this.reservations));
 }
 
 }
