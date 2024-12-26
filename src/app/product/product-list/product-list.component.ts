05098f62469f92d14d60158d9c972d32f84a6f21
Chetan Ananthu(UST <chetanananthu@gmail.com>
2024-12-26 16:36:36 +0530

    adding the search functionality

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -12,6 +12,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class ProductListComponent implements OnInit{
 
   products :Product []=[];
+  filteredProducts:Product[]=[];
   
   constructor(private productService:ProductService,
     private cartService:CartService,
@@ -21,6 +22,7 @@ export class ProductListComponent implements OnInit{
   ngOnInit(): void {
     this.productService.getProducts().subscribe(data=>{
       this.products=data;
+      this.filteredProducts=data;
     })
   }
 
@@ -35,6 +37,15 @@ export class ProductListComponent implements OnInit{
         });
       }
     })
+  }
+
+  applyFilter(event :Event):void{
+    let searchTerm=(event.target as HTMLInputElement).value;
+    searchTerm=searchTerm.toLocaleLowerCase();
+  
+    this.filteredProducts=this.products.filter(
+      product=>product.name.toLowerCase().includes(searchTerm)
+    )
   }
 
 }
